30035bf8617d701bdedb2bc582aa767c03253cb0
Alexis Peves <alexpeves@gmail.com>
2023-01-25 17:25:36 -0500

    url params and queries

--- java_practice/javaspring/demo/src/main/java/com/example/demo/controllers/HelloWorld.java
+++ java_practice/javaspring/demo/src/main/java/com/example/demo/controllers/HelloWorld.java
@@ -1,18 +1,17 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
 
-    @RequestMapping("/")
-    public String HelloWorld(){
-        return "Hello World";
+    @RequestMapping("/{hello}")
+    public String HelloWorld(@PathVariable(value="hello") String hello,String cheese){
+       
+        return hello+cheese;
     }
     
-    @RequestMapping("/cheese")
-    public String Cheese(){
-        return "Gouda";
-    }
+    
 }
