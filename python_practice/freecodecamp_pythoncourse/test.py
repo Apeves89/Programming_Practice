918af165aca04d11aba822ae0e2ce2354a1485fb
Alexis Peves <alexpeves@gmail.com>
2023-01-29 22:53:35 -0500

    string methods

---
+++ python_practice/freecodecamp_pythoncourse/test.py
@@ -0,0 +1,61 @@
+# name = ' Beau'
+# age = 39
+# gpa = 3.7
+
+#Possible variable syntax
+# name1 = 'john'
+# HEIGHT = '5ft 7in'
+# my_name = 'Alex'
+# _name = 'Scott'
+# print(type(age))        # returns type of variable
+# print(isinstance(name, str))
+
+# print(int("20"))              #converts string to integer, no decimals or text
+# print(float('20'))            # converts to float.
+
+1 +1 #2
+2 - 1 #1
+2 * 2 #4
+4 / 2 #2
+4 % 3 #1 modulo/modulus
+4 ** 2 #16 exponents
+5 // 2 #2 floor division, divides and rounds down to nearest whole number
+
+a = 1
+b = 2
+
+a == b #False
+a != b # True
+a > b #False
+a <= b #True
+
+name = 'Beau'
+# print("""Beau is
+#       39 
+#       years
+#       old""")
+# print(name.strip())
+# print("Beau".lower())
+# print("beAu pERson".title())
+# print("Beau".islower())
+# String Methods
+# isalpha()   #check if string contains only characters and is not empty
+# isalnum()   # check if a string contains characters or digits and is not empty
+# isdecimal() # check if a string contains digits and is not empty
+# lower()     # all characters set to lowercase
+# islower()   # check if string is all lowercase
+# upper()     # all characters set to uppercase
+# isupper()   # check if string is all uppercase
+# title()     #capitalizes first character of each word in string
+# startswith()# check if string starts with specific substring
+# endswith()  # check if string ends with specific substring
+# replace()   #replace a part of a string
+# split()     #splits a string by specified separator into a list
+# strip()     #trim the whitespace from a string
+# join()      #joins an iterable into one string
+# find()      #find the position of a substring, returns -1 if not found
+# len()       #returns the length of a string
+
+# name1 = "Be\nau"        # \ escape character
+print(name[1:3])        # ea
+
