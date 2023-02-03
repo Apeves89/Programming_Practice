d46a5a3edb4c42abef65d729de714350b3307f0d
Alexis Peves <alexpeves@gmail.com>
2023-02-03 18:21:46 -0500

    setup, variable, if/switch, for loops

---
+++ go_practice/alex-merced-go/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+var print = fmt.Println
+
+func main() {
+	// fmt.Println("Hello World")
+	// var myName string
+	// cheese := 5
+	// myName = "Alex Merced"
+	// print(myName, cheese)
+
+	// if 5 > 6 {
+	// 	print("This is true")
+	// } else {
+	// 	print("This is false")
+	// }
+
+	// for x := 0; x < 10; x++ {
+	// 	print(x)
+	// }
+
+	// x := 0 // similar to while loop
+	// for x < 10 {
+	// 	print(x)
+	// 	x++
+	// }
+
+	// x := 4
+	// switch x {
+	// case 1:
+	// 	print(1)
+	// case 2:
+	// 	print(2)
+	// default:
+	// 	print("None of the above")
+	// }
+
+}
